8d8158433b7546d775670e8215ddb27b425228f7
Collin Herber <rustaddicts@gmail.com>
2021-03-04 14:38:26 -0500

    chore: load bootstrap

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,8 @@ import {inject, route, Router} from 'aurelia';
 
 @route({
   routes: [
-    { id: 'home', path: '', component: import('./pages/home/home'), title: 'Home' },
-    { id: 'testpage', path: 'testpage', component: import('./pages/testpage/testpage'), title: 'Test Page' },
-    { id: '', path: ':404', component: import('./pages/home/home'), title: 'Home' },
+    { id: 'home', path: ['', 'home'], component: import('./pages/home/home'), title: 'Home' },
+    { id: 'testpage', path: 'testpage', component: import('./pages/testpage/testpage'), title: 'Test Page' }
   ]
 })
 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import Aurelia, {RouterConfiguration} from 'aurelia';
 import { App } from './app';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import '@popperjs/core';
 
 Aurelia
     .register(RouterConfiguration)
